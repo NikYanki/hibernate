38f7078900ab90da47c27807a7102b70c0e4363c
NikYanki <nankovoj799@gmail.com>
2022-06-28 19:17:59 +0200

    hw1.2

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,4 +1,4 @@
-import models.User;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.Metadata;
@@ -13,33 +13,14 @@ public class Test {
         StandardServiceRegistry standardServiceRegistry =new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml")
                 .build();
         Metadata metadata = new MetadataSources(standardServiceRegistry)
-                .addAnnotatedClass(User.class)
+                .addAnnotatedClass(null)
                 .getMetadataBuilder()
                 .build();
         SessionFactory sessionFactory = metadata
                 .getSessionFactoryBuilder()
                 .build();
         Session session = sessionFactory.openSession();
-//        session.beginTransaction();
-//
-//        session.save(new User("Mykola"));
-//        session.save(new User("Serj"));
-//
-//
-//        session.getTransaction().commit();
 
-
-//        List<User> users = session.createNativeQuery("select *  from User",User.class).getResultList();
-//        System.out.println(users);
-
-//        List<User> users = session.createQuery("select u  from User u",User.class).getResultList();
-//        System.out.println(users);
-        User user = session.find(User.class, 1);
-        User user1 = session.find(User.class, 2);
-        session.beginTransaction();
-        session.delete(user);
-        session.delete(user1);
-        session.getTransaction().commit();
 
 
         session.close();
